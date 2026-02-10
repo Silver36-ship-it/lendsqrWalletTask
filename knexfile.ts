50544f813577546bf9b4a8da55fc290802f2516b
Silver36-ship-it <emycallistus@gmail.com>
2026-02-10 14:24:27 +0100

    Remove hardcoded DB credentials, use env variables

--- knexfile.ts
+++ knexfile.ts
@@ -1,15 +1,16 @@
-// Update with your config settings.
-
 import type { Knex } from 'knex';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const config: { [key: string]: Knex.Config } = {
   development: {
     client: 'mysql2',
     connection: {
-      host: '127.0.0.1',
-      user: 'root',
-      password: 'SILVERkid,1234',
-      database: 'lendsqr_wallet'
+      host: mustGetEnv('DB_HOST'),
+      user: mustGetEnv('DB_USER'),
+      password: mustGetEnv('DB_PASSWORD'),
+      database: mustGetEnv('DB_NAME'), 
     },
 
  pool: { min: 2, max: 10 },
@@ -36,4 +37,13 @@ const config: { [key: string]: Knex.Config } = {
   }
 
 };
+
+function mustGetEnv(key: string): string {
+  const value = process.env[key];
+  if (!value) {
+    throw new Error(`Missing environment variable: ${key}`);
+  }
+  return value;
+}
+
 export default config;
